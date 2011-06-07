4766ecae0ba34dd4f6de1f535e0a24dece6bbc4d
mani <000000mani@gmail.com>
2011-06-07 17:37:10 +0800

    intermediate checkin

--- WhatNEXT/WhatNEXT/TaskReminderUtility.cs
+++ WhatNEXT/WhatNEXT/TaskReminderUtility.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace WhatNEXT
 {
@@ -9,10 +10,20 @@ namespace WhatNEXT
     {
         public static void RemindTask(string taskDetails)
         {
-
             ITaskParser taskParser = TaskListFactory.GetInstance().CreateTaskParser();
+            TaskItem taskItem = taskParser.Parse("taskDetails");
 
-            taskParser.Parse("");
+            ITaskList list = TaskListFactory.GetInstance().CreateList();
+            list.AddTask(taskItem);
+
+            TaskScheduler taskScheduler = new TaskScheduler();
+            CommandInterpreter commandInterpreter = new CommandInterpreter(taskScheduler);
+            
+            Console.WriteLine("Main Thread id: {0}", Thread.CurrentThread.ManagedThreadId);
+            Thread.CurrentThread.Join(5000);
+
+
+            
         }
     }
 }
--- WhatNEXT/WhatNEXT/TaskScheduler.cs
+++ WhatNEXT/WhatNEXT/TaskScheduler.cs
@@ -9,33 +9,48 @@ namespace WhatNEXT
     public class TaskScheduler
     {
         public event TaskSchedulerEventHandler Schedule;
+        private static TaskScheduler taskScheduler;
+
+        public static TaskScheduler GetTaskScheduler()
+        {
+            if (taskScheduler == null)
+            {
+                taskScheduler = new TaskScheduler();
+            }
+            return taskScheduler;
+        }
 
         public static void Main()
         {
-            TaskScheduler taskScheduler = new TaskScheduler();
-            CommandInterpreter commandInterpreter = new CommandInterpreter(taskScheduler);
-            taskScheduler.ScheduleTask();
-            Console.WriteLine("Main Thread id: {0}", Thread.CurrentThread.ManagedThreadId);
-            Thread.CurrentThread.Join(5000);
-
+            //TaskScheduler taskScheduler = new TaskScheduler();
+            //CommandInterpreter commandInterpreter = new CommandInterpreter(taskScheduler);
+            //taskScheduler.ScheduleTask();
+            //Console.WriteLine("Main Thread id: {0}", Thread.CurrentThread.ManagedThreadId);
+            //Thread.CurrentThread.Join(5000);
         }
         // Invoke the Changed event; called whenever list changes
         protected virtual void OnSchedule(object e)
         {
             if (Schedule != null)
             {
-
                 Schedule(this, (TaskScheduleEventArgs)e);
             }
+        }
+        private TaskScheduler()
+        {
+            ITaskList list = TaskListFactory.GetInstance().CreateList();
+
+            ((ITaskListWithEvents)list).Add += new AddTaskEventHandler(TaskScheduler_Add);
 
         }
-        public TaskScheduler()
+        void TaskScheduler_Add(object sender, TaskAddEventArgs e)
         {
-
+             ScheduleTask(taskItem)
         }
-        public void ScheduleTask()
+        public void ScheduleTask(TaskItem taskItem)
         {
-          
+            Timer taskTimer = new Timer(new TimerCallback(OnSchedule),
+                                        new TaskScheduleEventArgs(taskItem), taskItem.TimeReminder, Timeout.Infinite);
         }
         public void ScheduleDummyTask()
         {
